4d5af34ed9aaf5ccd43fe94bf6010446b7cb5366
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-07-16 10:36:38 +0530

    socket created

---
+++ index.js
@@ -0,0 +1,21 @@
+const {createServer} = require('node:http')
+const express = require("express")
+const path = require("path")
+const {Server} = require("socket.io")
+
+// socket.io
+const app = express()
+const server = createServer(app)
+const io  = new Server(server) 
+
+
+io.on('connection',(socket)=>{
+    console.log('A new user has connected',socket.id)
+})
+
+app.use(express.static(path.resolve("./public")))
+app.get("/",(req,res)=>{
+    return res.sendFile("./public/index.html")
+})
+
+server.listen(9000,()=> console.log(`server started at PORT:9000`))
